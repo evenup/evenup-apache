128e5ffbee164739c29cb8f251c0df6c94c1aa15
Justin Lambert <jlambert@eml.cc>
2013-08-31 19:04:47 -0600

    spec update

--- spec/classes/apache_passenger_spec.rb
+++ spec/classes/apache_passenger_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'apache::passenger', :type => :class do
 
   it { should create_class('apache::passenger') }
+  it { should include_class('ruby::rack') }
   it { should contain_package('mod_passenger') }
   it { should contain_apache__cfgfile('passenger') }
 
