782b44b819cc000321d92f3dc5e7a6c8dd76ae3d
Justin Lambert <jlambert@eml.cc>
2014-02-04 20:57:06 -0700

    allow setting apache version

--- spec/classes/apache_install_spec.rb
+++ spec/classes/apache_install_spec.rb
@@ -1,18 +1,27 @@
 require 'spec_helper'
 
-describe 'apache::install', :type => :class do
+describe 'apache', :type => :class do
 
   it { should create_class('apache::install') }
 
-  it { should contain_package('httpd') }
+  context 'install' do
+    context 'default' do
+      it { should contain_package('httpd').with_ensure('latest') }
 
-  it { should contain_file('/etc/httpd/conf/httpd.conf') }
-  it { should contain_file('/etc/httpd/conf.d/').with(
-    'ensure'  => 'directory',
-    'purge'   => true,
-    'recurse' => true,
-    'force'   => true
-  ) }
-  it { should contain_file('/var/log/httpd/') }
+      it { should contain_file('/etc/httpd/conf/httpd.conf') }
+      it { should contain_file('/etc/httpd/conf.d/').with(
+        'ensure'  => 'directory',
+        'purge'   => true,
+        'recurse' => true,
+        'force'   => true
+      ) }
+      it { should contain_file('/var/log/httpd/') }
+    end
+
+    context '#ensure' do
+      let(:params) { { :ensure => '2.3.4' } }
+      it { should contain_package('httpd').with_ensure('2.3.4') }
+    end
+  end
 
 end
